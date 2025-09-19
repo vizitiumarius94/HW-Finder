6ffa71154380f5e9f7dd18b8138abbba91dfe2d8
vizitiumarius94 <53187477+vizitiumarius94@users.noreply.github.com>
2025-09-19 20:26:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const importBtn = document.getElementById('importBtn');
 const importFile = document.getElementById('importFile');
 
 // ------------------- FETCH DATA -------------------
-fetch('data.x')
+fetch('data.json')
   .then(res => res.json())
   .then(data => carsData = data);
 
@@ -264,7 +264,7 @@ exportBtn.addEventListener('click', () => {
   const url = URL.createObjectURL(blob);
   const a = document.createElement('a');
   a.href = url;
-  a.download = 'hotwheels_data.x';
+  a.download = 'hotwheels_data.json';
   a.click();
   URL.revokeObjectURL(url);
 });
