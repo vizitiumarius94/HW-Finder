b5c0f8d315eb6c459ab6c73f9560f8937ff9c456
vizitiumarius94 <53187477+vizitiumarius94@users.noreply.github.com>
2025-10-21 11:57:19 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "garage-cache-v18";
+const CACHE_NAME = "garage-cache-v19";
 const DATA_CACHE = "dynamic-data"; // dedicated cache for data.json
 const CORE_ASSETS = [
   "index.html",
